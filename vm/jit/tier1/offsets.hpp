81d3ec0c8bd8557a0c15471b30f54df75dbebe93
Evan Phoenix <evan@fallingsnow.net>
2011-10-17 09:17:07 -0700

    Add tier1 offsets file

---
+++ vm/jit/tier1/offsets.hpp
@@ -0,0 +1,14 @@
+namespace rubinius {
+namespace tier1 {
+namespace offsets {
+  namespace Arguments {
+    int name = 0x0;
+    int recv = 0x8;
+    int block = 0x10;
+    int total = 0x18;
+    int arguments = 0x1c;
+    int tuple = 0x24;
+  }
+}
+}
+}
